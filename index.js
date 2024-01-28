fa45879c578edcc8a24669f84d7230d32540ac75
Archeb <archebasic@hotmail.com>
2024-01-28 12:21:40 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Telegraf, Markup } = require("telegraf");
-import { message } from 'telegraf/filters';
+const { message } = 'telegraf/filters';
 var yauzl = require("yauzl");
 const superagent = require("superagent");
 const queue = require("promise-queue-plus");
